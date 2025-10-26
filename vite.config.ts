e4ee6b50edb8d71fba21da6a4b814b47fe640999
misu <62200549+mihailnica10@users.noreply.github.com>
2025-10-26 16:49:50 +0000

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()].filter(Boolean),
-  base: "/bringo-order-report/",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
